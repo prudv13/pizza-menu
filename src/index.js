d2d7c710a45442d751f97e4e09d8d7c3e371a551
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-13 11:31:09 +0530

    conditional rendering with ternaries

--- src/index.js
+++ src/index.js
@@ -72,13 +72,14 @@ function Menu(){
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            {numPizzas > 0 && (
+            {numPizzas > 0 ? (
                     <ul className="pizzas">
                         {pizzas.map(pizza => 
                         <Pizza pizzaObj={pizza} key={pizza.name} />
                         )}
                     </ul>
                 )
+                : <p>We're still working on our menu. Please come back later :)</p>
             }
         </main>
     );
@@ -99,7 +100,7 @@ function Pizza(props) {
 
 function Footer(){
     const hour = new Date().getHours();
-    const openHour = 10;
+    const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
     console.log(isOpen);
@@ -109,13 +110,14 @@ function Footer(){
     
     return (
         <footer className="footer">
-            {isOpen && (
+            {isOpen ? (
                 <div className="order">
                     <p>We're open until {closeHour}:00. 
                     Come Visit us or order online.</p>
                     <button className="btn">Order</button>
                 </div>
-                )
+                ) 
+                : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>
             }
         </footer>
     );
