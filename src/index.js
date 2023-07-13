fba674ca11420168aa55c30c04db966284d6b476
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-13 11:23:37 +0530

    conditional rendering with AND operator

--- src/index.js
+++ src/index.js
@@ -67,14 +67,19 @@ function Header(){
 }
 
 function Menu(){
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            <ul className="pizzas">
-                {pizzaData.map(pizza => 
-                    <Pizza pizzaObj={pizza} key={pizza.name} />
-                )}
-            </ul>
+            {numPizzas > 0 && (
+                    <ul className="pizzas">
+                        {pizzas.map(pizza => 
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                        )}
+                    </ul>
+                )
+            }
         </main>
     );
 }
@@ -94,7 +99,7 @@ function Pizza(props) {
 
 function Footer(){
     const hour = new Date().getHours();
-    const openHour = 12;
+    const openHour = 10;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
     console.log(isOpen);
@@ -104,7 +109,14 @@ function Footer(){
     
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()}. We're currently open!
+            {isOpen && (
+                <div className="order">
+                    <p>We're open until {closeHour}:00. 
+                    Come Visit us or order online.</p>
+                    <button className="btn">Order</button>
+                </div>
+                )
+            }
         </footer>
     );
 }
