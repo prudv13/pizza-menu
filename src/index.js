3108911d5339e86a32e33e018443961b12f6ab84
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-13 11:51:30 +0530

    destructuring props

--- src/index.js
+++ src/index.js
@@ -85,15 +85,15 @@ function Menu(){
     );
 }
 
-function Pizza(props) {
-    if(props.pizzaObj.soldOut) return null;
+function Pizza({pizzaObj}) {
+    if(pizzaObj.soldOut) return null;
     return (
         <li className="pizza">
-            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
-                <h3>{props.pizzaObj.name}</h3>
-                <p>{props.pizzaObj.ingredients}</p>
-                <span>{props.pizzaObj.price + 3}</span>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                <span>{pizzaObj.price + 3}</span>
             </div>
         </li>
     );
@@ -120,7 +120,7 @@ function Footer(){
     return (
         <footer className="footer">
             {isOpen ? (
-                <Order closeHour={closeHour} />
+                <Order closeHour={closeHour} openHour={openHour} />
                 ) 
                 : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
             }
@@ -128,11 +128,11 @@ function Footer(){
     );
 }
 
-function Order(props){
+function Order({closeHour, openHour}){
     return (
         <div className="order">
             <p>
-            We're open until {props.closeHour}:00.
+            We're open from {openHour}:00 to {closeHour}:00.
             Come Visit us or order online.
             </p>
             <button className="btn">Order</button>
